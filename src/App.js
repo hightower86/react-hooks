38ad81e583b50fdbe1526e7f5b443c522db7f844
Adel <htwr86@gmail.com>
2019-10-29 23:07:21 +0300

    useState in App component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React, { Component } from 'react'
-import TodoList from './TodoList'
+import React, { useState } from 'react';
+import TodoList from './TodoList';
 
-export default class App extends Component {
-  state = {
-    todos: [
-      {id: 1, title: 'First todo', completed: false},
-      {id: 2, title: 'Second todo', completed: true},
-    ]
-  }
+const App = () => {
+  const [todos, setTodos] = useState([
+    { id: 1, title: 'First todo', completed: false },
+    { id: 2, title: 'Second todo', completed: true }
+  ]);
 
-  render() {
-    return (
-      <div className="container">
-        <h1>Todo app</h1>
+  const [todoTitle, setTodoTitle] = useState('');
 
-          <div className="input-field">
-            <input type="text" />
-            <label>Todo name</label>
-          </div>
+  const addTodo = e => {
+    if (e.key === 'Enter') {
+      setTodos([
+        ...todos,
+        {
+          id: Date.now(),
+          title: todoTitle,
+          completed: false
+        }
+      ]);
+      setTodoTitle('');
+    }
+  };
 
-          <TodoList todos={this.state.todos} />
+  return (
+    <div className='container'>
+      <h1>Todo app</h1>
+
+      <div className='input-field'>
+        <input
+          type='text'
+          value={todoTitle}
+          onChange={event => setTodoTitle(event.target.value)}
+          onKeyPress={addTodo}
+        />
+        <label>Todo name</label>
       </div>
-    );
-  }
-}
+
+      <TodoList todos={todos} />
+    </div>
+  );
+};
+export default App;
