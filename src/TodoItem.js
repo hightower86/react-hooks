703dd705c337e34800bfe44e5f5a38fa2eebbefc
Adel <htwr86@gmail.com>
2019-10-30 23:01:31 +0300

    useContext

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,23 +1,33 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
+import { Context } from './context';
 
-const onDelete = id => {
-  console.log('onDelete', id);
-};
+// const onDelete = id => {
+//   console.log('onDelete', id);
+// };
 
 export default function TodoItem({ title, id, completed }) {
   const [checked, setChecked] = useState(completed);
-  const onInputChange = () => {
-    setChecked(!checked);
-  };
+
+  const [toggleTodo, removeTodo] = useContext(Context);
+
+  // const onInputChange = () => {
+  //   setChecked(!checked);
+  // };
   const cls = ['todo'];
-  if (checked) cls.push('completed');
+
+  if (completed) cls.push('completed');
+
   return (
     <li className={cls.join(' ')}>
       <label>
-        <input type='checkbox' checked={checked} onChange={onInputChange} />
+        <input
+          type='checkbox'
+          checked={completed}
+          onChange={() => toggleTodo(id)}
+        />
         <span>{title}</span>
 
-        <i className='material-icons red-text' onClick={() => onDelete(id)}>
+        <i className='material-icons red-text' onClick={() => removeTodo(id)}>
           delete
         </i>
       </label>
